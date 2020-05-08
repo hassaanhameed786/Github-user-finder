ab06fd8208030ca7ba5caab57f2ca941c86b0012
frozenheart3k <31187421+hassaanhameed786@users.noreply.github.com>
2020-05-09 02:05:06 +0500

    Add files via upload

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+export default function About() {
+    return (
+        <Fragment>
+        <h1>About</h1>
+        <p>it all about to find Github users</p>
+        <p>Version 1.0.2.5</p>
+            
+        </Fragment>
+    )
+}
