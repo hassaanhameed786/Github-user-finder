1ccc26b76ef7a3b55f9d510af37e1acf8004f07a
frozenheart3k <31187421+hassaanhameed786@users.noreply.github.com>
2020-05-09 02:09:10 +0500

    Update Useritems.js

--- src/components /users/Useritems.js
+++ src/components /users/Useritems.js
@@ -1,28 +1,59 @@
-import React from 'react'
-import PropTypes from 'prop-types' 
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-const Useritems = ({user:{login,avatar_url,html_url}}) =>{ 
-        
+
+export class Search extends Component {
+
+    state ={
+        text:''
+
+    };
+
+    //propTypes
+
+    static propTypes={
+        SearchGitUsers:PropTypes.func.isRequired,
+        clearUsers:PropTypes.func.isRequired,
+        Showclear:PropTypes.bool.isRequired,
+        set_alert:PropTypes.func.isRequired
+
+    }
+
+    onSubmit=(e) =>{
+        e.preventDefault();
+        if (this.state.text === '') {
+            
+            this.props.set_alert(' Please Enter something ' , 'light');
+                    
+        }
+
+        else{
+
+            this.props.SearchGitUsers(this.state.text);
+            this.setState({text:''});
+
+        }
+    }
+
+    onChange = (e) =>{
+        this.setState({text:e.target.value});
+    }
+
+    render() {
+       // const { clearUsers , Showclear} = this.props
         return (
-            <div className="card text-center">
-            
-            <img src={avatar_url} alt=""  className="round-img" style={{
-                width:'60px'
-            }}></img>
-
-            <h3>{login}</h3>
-            
             <div>
-
-            <a href={html_url} className="btn btn-dark btn-sm"> More </a>
+            <form onSubmit={this.onSubmit} className="form">
+                <input type="text" name="text" placeholder="Search users..." value={this.state.text}  onChange={this.onChange}/>
+                <input type="submit" value="Search" className="btn btn-dark btn-block" />
+            </form>
+            {this.props.Showclear && 
+             
+            <button className="btn btn-dark btn-block" onClick={this.props.clearUsers}>Clear</button>
+            }
             </div>
-            </div>
-        );
-    
+        )
+    }
 }
-Useritems.prototype ={
-    user:PropTypes.object.isRequired,   // ptor <== Hotkey 
 
-};
-
-export default Useritems
+export default Search 
