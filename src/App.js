815a30e5b6c3d1e601738142dd8cde4daf1a6ddf
frozenheart3k <31187421+hassaanhameed786@users.noreply.github.com>
2020-05-08 23:20:25 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,64 +1,103 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import {BrowserRouter as Router ,Switch ,Route} from 'react-router-dom';
 import Navbar from './components /layout /Navbar';
 import axios from 'axios';
 import './App.css';
-
-
+import About from './components /pages/About';
+import Alert from './components /layout /Alert';
 import Users from './components /users/Users';
+import User from './components /users/User';
 import Search from './components /users/Search';
 class App extends Component {
   // eslint-disable-next-line
 
+  state = {
+    users:[],
+    loading: false,
+    alert:null,
+    user: {}
+  }
 
   //Seraching Git users s
 
   SearchGitUsers = async text =>{
     this.setState({loading:true});
 
-    this.setState({loading:true});
+
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID} & client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
    //console.log(res.data)
    this.setState({users :res.data.items, loading:false});
   }
 
-  //now use State & make a arrray of users  for  gettin via api.github
-  state = {
-    users:[],
-    loading: false
+  ///Get single users from 
+
+  getUser =async (username) => 
+
+  {
+    this.setState({loading:true});
+    const res = await axios.get(`https://api.github.com/search/users/${username}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID} & client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+   //console.log(res.data)
+   this.setState({user :res.data, loading:false});
+
   }
 
-  // takes a http request for github api 
-  // async componentDidMount(){
-
-  //   console.log(process.env.REACT_APP_GITHUB_CLIENT_ID);
-
-  //   this.setState({loading:true});
-  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID} & client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-  //  //console.log(res.data)
-  //  this.setState({users :res.data, loading:false});
-  // }
-
-
   //clears users form the state 
-  clearUsers =() => this.setState({users:[], loading:false})
+  
   
 
 
   render() {
+
+    
     return (
-      <div > 
+      <Router>
+      <div> 
 
         <Navbar /> 
-        <Search clearUsers={this.clearUsers} SearchGitUsers={this.SearchGitUsers}/>
 
 
         <div className="container">
-        <Users loading={this.state.loading}  users={this.state.users}/>
+        <Alert alert={this.setState.alert}/>
+
+        <Switch>
+        <Route exact path="/" render={props =>(
+
+          <Fragment>
+
+          <Search 
+          
+          SearchGitUsers={this.SearchGitUsers} 
+          clearUsers={this.clearUsers}
+          Showclear={this.users }
+          set_alert={this.set_alert}       /> 
+          <Users loading={this.state.loading}  users={this.state.users}/>
+        </Fragment>
         
+        )}
+        
+        />
+        
+
+
+        <Route exact path='/about' component={About} />     
+        <Route   exact path='/user/:login ' render={ props =>(
+
+          <User {...props} getUser={this.getUser} user={this.state.user} loading={this.state.loading} />
+         )} >
+        
+        
+        
+        
+         </Route>
+
+        </Switch>
+
+
         </div>
       </div>
 
-     
+      </Router>
+
     );
   }
 }
